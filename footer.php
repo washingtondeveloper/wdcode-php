3c29cf4ecc1145912fcdc4252a839055050923ca
washingtondeveloper <washingtonprogramador@gmail.com>
2017-09-12 14:00:49 -0300

    aplicação php

---
+++ footer.php
@@ -0,0 +1,7 @@
+	
+	<footer  class="text-center">
+		<small>&copy;Washington Development - 2017</small>
+	</footer>
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include('header.php') ?>
+
+	<div class="container">
+		
+		<div class="jumbotron">
+			<h1>Agenda de Contatos</h1>
+			<p>Aplicação desenvolvida em PHP 7</p>
+		</div>
+
+	</div>
+
+<?php include('footer.php') ?>
